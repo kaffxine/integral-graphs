d53f8808c3f63a097648bc0c35f42c78c8a7d787
kavusha <kaffxine@gmail.com>
2025-02-10 17:42:10 +0100

    added a simple text-based interface for monitoring the simulation

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,115 @@ pub use matrix::Matrix;
 pub mod database;
 pub use database::Database;
 
+use std::io::{self, Write};
 use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::mem::size_of::<usize>() != 8 {
         panic!("do not run this program on non-64-bit arch");
     }
 
-    test_generate();
+    let db = Arc::new(Mutex::new(Database::new()));
+    let stats = Arc::new(Mutex::new((0, 0, 0)));
 
+    let interact_handle = {
+        let db = db.clone();
+        let stats = stats.clone();
+        thread::spawn(move || interact(db, stats))
+    };
+
+    let generate_handle = {
+        let db = db.clone();
+        let stats = stats.clone();
+        thread::spawn(move || keep_generating(13, 4, db, stats))
+    };
+
+    interact_handle.join().unwrap();
+    generate_handle.join().unwrap();
 
     Ok(())
 }
 
-fn keep_generating(n_nodes: u64, max_degree: u32, db: Arc<Mutex<Database>>) -> Result<(), String> {
+const FILE_PATH: &str = "/tmp/integralgraphsimulationdbfilepath";
+const INTERACT_MESSAGE: &str = r#"Enter a command to perform an action:
+  S -> show the stats
+  Q -> quit
+Input: "#;
+
+fn interact(
+    db: Arc<Mutex<Database>>,
+    stats: Arc<Mutex<(u128, u128, u128)>>,
+) -> Result<(), String> {
+    loop {
+        print!("{}", INTERACT_MESSAGE);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+        match command {
+            "S" => {
+                let guard = stats.lock().unwrap();
+                let (n_generated, n_in_db, n_integral) = *guard;
+                println!("[{n_generated}] graphs generated");
+                println!("[{n_in_db}] unique graphs stored in the database");
+                println!("[{n_integral}] integral graphs found so far");
+            },
+            "Q" => {
+                std::process::exit(0);
+            }
+            _ => {
+                println!("Wrong command!");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn keep_generating(
+    n_nodes: u64,
+    max_degree: u32,
+    db: Arc<Mutex<Database>>,
+    stats: Arc<Mutex<(u128, u128, u128)>>,
+) -> Result<(), String> {
+    let mut seed = 0;
+    let (mut n_generated, mut n_in_db, mut n_integral) = {
+        let guard = stats.lock().unwrap();
+        guard.clone()
+    };
+    loop {
+        seed += 1;
+        {
+            let mut guard = stats.lock().unwrap();
+            *guard = (n_generated.clone(), n_in_db.clone(), n_integral.clone());
+        };
+
+        let adjm = generate(n_nodes, max_degree, seed)?;
+        let labeled = labelg(adjm.to_graph6()?)?;
+
+        n_generated += 1;
+
+        {
+            let guard = db.lock().unwrap();
+            if let Some(_) = guard.get(&labeled) {
+                continue;
+            }
+        }
+
+        n_in_db += 1;
+
+        if is_integral(adjm)? {
+            n_integral += 1;
+            let mut guard = db.lock().unwrap();
+            guard.insert(labeled.clone(), true);
+        } else {
+            let mut guard = db.lock().unwrap();
+            guard.insert(labeled.clone(), false);
+        }
+    }
 
     Ok(())
 }
